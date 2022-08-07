639e3aec2cf809ba7ed1826ac7eaa6dba47a89d7
Blake Sanie <38799390+blakesanie@users.noreply.github.com>
2022-08-07 15:16:20 -0500

    Delete functions directory

--- functions/functions/stripe/index.js
+++
@@ -1,6 +0,0 @@
-import admin from "firebase-admin";
-import { getAuth } from "firebase-admin/auth";
-import functions from "firebase-functions";
-import { logger } from "firebase-functions";
-import Stripe from "stripe";
-
